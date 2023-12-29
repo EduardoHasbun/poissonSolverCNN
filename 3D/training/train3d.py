9a3b6b5018bdb30cb2578dc887e3a0ebf8c70f48
Eduardo Hasbun <hasbun.eduardo1999@gmail.com>
2023-12-29 07:13:07 -0300

    mc

--- 3D/training/train3d.py
+++ 3D/training/train3d.py
@@ -57,7 +57,7 @@ for epoch in range (num_epochs):
     for batch_idx, batch in enumerate(dataloader):
         data = batch[:, np.newaxis, :, :]
         optimizer.zero_grad()
-        data = data.to(model.dtype)
+        data = data.to(model.parameters().__next__().dtype)
         optimizer.zero_grad()
         data_norm = torch.ones((data.size(0), data.size(1), 1, 1)) / ratio_max
 
