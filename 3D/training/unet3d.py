01f9afd4e715b7687cc3ec27b2cc3ebf5a5ab3c0
Eduardo Hasbun <hasbun.eduardo1999@gmail.com>
2024-01-17 18:53:45 -0300

    changes in loss and model

--- 3D/training/unet3d.py
+++ 3D/training/unet3d.py
@@ -41,10 +41,10 @@ class UNet3D(nn.Module):
             _ConvBlock3D(scales[3][0], kernel, pool=True),
         ])
 
-        self.ConvBottom = _ConvBlock3D(scales[4][0], kernel, pool=True, upsample_size=(int(input_res/9), int(input_res/9), int(input_res/2)))
+        self.ConvBottom = _ConvBlock3D(scales[4][0], kernel, pool=True, upsample_size=(int(input_res/9), int(input_res/9), int(input_res/9)))
 
         self.ConvsUp = nn.ModuleList([
-            _ConvBlock3D(scales[1][1], kernel, upsample_size=(int(input_res/9), int(input_res/4), int(input_res/2))),
+            _ConvBlock3D(scales[1][1], kernel, upsample_size=(int(input_res/4), int(input_res/4), int(input_res/2))),
             _ConvBlock3D(scales[2][1], kernel, upsample_size=(int(input_res/2), int(input_res), int(input_res*2))),
             _ConvBlock3D(scales[3][1], kernel, upsample_size=(int(input_res), int(input_res*2), int(input_res*4))),
             _ConvBlock3D(scales[0][1], kernel, last_one=True),
