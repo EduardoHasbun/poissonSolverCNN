b9f54ef588d78c812a73ce96731b83cc07ae0ee2
Eduardo Hasbun <hasbun.eduardo1999@gmail.com>
2024-03-01 09:38:53 -0300

    msnet

--- 3D/training/msnet3d.py
+++ 3D/training/msnet3d.py
@@ -34,6 +34,7 @@ class MSNet3D(nn.Module):
         # For upsample the list of resolution is needed when 
         # the number of points is not a power of 2
         self.input_res = tuple([input_res, input_res])
+        print('this')
         self.list_res = [int(input_res / 2**i) for i in range(self.n_scales)]
 
         # create down_blocks, bottom_fmaps and up_blocks
