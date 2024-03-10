3454f71691d05c7c1e1c542a27ee0946d8f53a2d
Eduardo Hasbun <hasbun.eduardo1999@gmail.com>
2024-03-10 18:39:47 -0300

    msnet

--- 3D/training/msnet3d.py
+++ 3D/training/msnet3d.py
@@ -10,6 +10,7 @@ class _ConvBlock3D(nn.Module):
         layers = list()
         # Append all the specified layers
         for i in range(len(fmaps) - 1):
+            print(kernel_size)
             layers.append(nn.Conv3d(fmaps[i], fmaps[i + 1], 
                 kernel_size=kernel_size, padding=int((kernel_size - 1) / 2),
                 padding_mode=padding_mode))
