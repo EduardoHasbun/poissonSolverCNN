b182c522c8cb130357cb49b04b3cd5c3d5ef0806
Eduardo Hasbun <hasbun.eduardo1999@gmail.com>
2024-03-12 22:43:06 -0300

    new unet

--- 3D/training/train3d.py
+++ 3D/training/train3d.py
@@ -63,6 +63,8 @@ elif model_type == 'MSNet':
     print('Using MSNet model')
 else:
     print('No model found')
+
+print(model)
 model = model.float() 
 laplacian_loss = LaplacianLoss(cfg, lapl_weight=lapl_weight)
 dirichlet_loss = DirichletBoundaryLoss(bound_weight)
