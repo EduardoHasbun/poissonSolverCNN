f81ac61904e616a8bde6fe787837e44d44a2d7df
Eduardo Hasbun <hasbun.eduardo1999@gmail.com>
2024-03-12 23:36:44 -0300

    new unet

--- 3D/training/unet3d.py
+++ 3D/training/unet3d.py
@@ -4,11 +4,11 @@ import torch.nn.functional as F
 
 
 class CustomPadLayer3D(nn.Module):
-    def __init__(self):
+    def __init__(self, kernel_sizes):
         super(CustomPadLayer3D, self).__init__()
-        self.padx = int((self.kernel_sizes[2] - 1) / 2)
-        self.pady = int((self.kernel_sizes[1] - 1) / 2)
-        self.padz = int((self.kernel_sizes[0] - 1) / 2)
+        self.padx = int((kernel_sizes[2] - 1) / 2)
+        self.pady = int((kernel_sizes[1] - 1) / 2)
+        self.padz = int((kernel_sizes[0] - 1) / 2)
 
     def forward(self, x):
         x = F.pad(x, (self.padx, self.padx, self.pady, self.pady, self.padz, self.padz), "constant", 0)
@@ -16,7 +16,7 @@ class CustomPadLayer3D(nn.Module):
 
 
 class _ConvBlock3D(nn.Module):
-    def __init__(self, fmaps, block_type, kernel_size, padding_mode='zeros', upsample_mode='nearest', out_size=None):
+    def __init__(self, fmaps, block_type, kernel_size, kernel_sizes, upsample_mode='nearest', out_size=None):
         super(_ConvBlock3D, self).__init__()
         layers = []
         # Apply pooling on down and bottom blocks
@@ -25,7 +25,7 @@ class _ConvBlock3D(nn.Module):
 
         # Append all the specified layers
         for i in range(len(fmaps) - 1):
-            layers.append(CustomPadLayer3D())
+            layers.append(CustomPadLayer3D(kernel_sizes))
             layers.append(nn.Conv3d(fmaps[i], fmaps[i + 1], kernel_size=kernel_size, padding=0))
             layers.append(nn.ReLU())
 
@@ -49,22 +49,22 @@ class UNet3D(nn.Module):
 
         # Entry layer
         self.ConvsDown = nn.ModuleList()
-        self.ConvsDown.append(_ConvBlock3D(self.scales[0][0], 'in', kernel_sizes[0]))
+        self.ConvsDown.append(_ConvBlock3D(self.scales[0][0], 'in', kernel_sizes[0], kernel_sizes))
 
         # Intermediate down layers (with MaxPool at the beginning)
         for i in range(1, self.max_scale):
-            self.ConvsDown.append(_ConvBlock3D(self.scales[i][0], 'down', kernel_sizes[i]))
+            self.ConvsDown.append(_ConvBlock3D(self.scales[i][0], 'down', kernel_sizes[i], kernel_sizes))
 
         # Bottom layer (MaxPool at the beginning and Upsample/Deconv at the end)
-        self.ConvBottom = _ConvBlock3D(self.scales[self.max_scale][0], 'bottom', kernel_sizes[self.max_scale])
+        self.ConvBottom = _ConvBlock3D(self.scales[self.max_scale][0], 'bottom', kernel_sizes[self.max_scale], kernel_sizes)
 
         # Intemediate layers up (UpSample/Deconv at the end)
         self.ConvsUp = nn.ModuleList()
         for i in range(self.max_scale - 1, -1, -1):
-            self.ConvsUp.append(_ConvBlock3D(self.scales[i][1], 'up', kernel_sizes[i]))
+            self.ConvsUp.append(_ConvBlock3D(self.scales[i][1], 'up', kernel_sizes[i], kernel_sizes))
 
         # Out layer
-        self.ConvsUp.append(_ConvBlock3D(self.scales[0][1], 'out', kernel_sizes[0]))
+        self.ConvsUp.append(_ConvBlock3D(self.scales[0][1], 'out', kernel_sizes[0], kernel_sizes))
 
     def forward(self, x):
         # List of the temporary x that are used for linking with the up branch
