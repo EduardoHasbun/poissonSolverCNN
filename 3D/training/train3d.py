941cd94c205b7c5e1acccd8556edb2b53d662b41
Eduardo Hasbun <hasbun.eduardo1999@gmail.com>
2024-05-01 23:58:26 -0400

    laplacian ratio all

--- 3D/training/train3d.py
+++ 3D/training/train3d.py
@@ -49,6 +49,7 @@ ratio_max = ratio_potrhs(alpha, Lx, Ly, Lz)
 
 #Create Data
 dataset = np.load(data_dir)
+dataset = dataset * ratio_max
 # dataset = np.tile(dataset, (1000, 1, 1, 1))
 dataset = torch.tensor(dataset)
 if loss_type == 'inside':
@@ -106,4 +107,4 @@ for epoch in range (num_epochs):
         if batch_idx % 10 ==0:
             print(f"Epoch {epoch}, Batch {batch_idx}, Loss: {loss.item()}")
     print(f"Epoch [{epoch + 1}/{num_epochs}] - Loss: {total_loss / len(dataloader)}")
-    torch.save(model.state_dict(), os.path.join(save_dir, 'laplacian_loss.pth'))
+    torch.save(model.state_dict(), os.path.join(save_dir, 'laplacian_loss_ratio_all.pth'))
