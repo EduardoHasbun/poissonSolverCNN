415790a341a025323bd466f9e8a4b2c7898c5380
Eduardo Hasbun <hasbun.eduardo1999@gmail.com>
2024-05-09 22:32:36 -0400

    laplacian testing

--- 3D/training/operators3d.py
+++ 3D/training/operators3d.py
@@ -65,6 +65,7 @@ class NewDirichletBoundaryLoss(nn.Module):
         # Concatenating along the appropriate dimension
         domain = torch.cat((x, y, z), dim=2)
         print(np.shape(domain))
+        print(np.shape[0,0,0,:,0])
 
         # Compute the boundary condition for each boundary
         top = function2solve(x, y, torch.ones_like(z)*self.zmax)
