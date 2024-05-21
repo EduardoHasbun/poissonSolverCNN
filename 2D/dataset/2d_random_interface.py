ac4d750be6693a2eabeb78b0ba462a4d70d1f02c
Eduardo Hasbun <hasbun.eduardo1999@gmail.com>
2024-05-21 01:27:29 -0400

    interface

--- 2D/dataset/2d_random_interface.py
+++ 2D/dataset/2d_random_interface.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import argparse
 from scipy.interpolate import RegularGridInterpolator as rgi
 from tqdm import tqdm as log_progress
+from multiprocessing import Pool, cpu_count
 
 
 # Specific arguments
@@ -15,22 +16,28 @@ with open(args.cfg, 'r') as yaml_stream:
     cfg = yaml.safe_load(yaml_stream)
 nits = cfg['n_it']
 ploting = False
+# Parameters for data generation
+xmin, xmax, nnx = cfg['domain']['xmin'], cfg['domain']['xmax'], cfg['domain']['nnx']
+nny, ymin, ymax = cfg['domain']['nny'], cfg['domain']['ymin'], cfg['domain']['ymax']
+interface_center = (cfg['domain']['interface_center']['x'], cfg['domain']['interface_center']['y'])
+interface_radius = cfg['domain']['interface_radius']
+n_res_factor = 20
+# Create a grid
+x, y= np.linspace(xmin, xmax, nnx), np.linspace(ymin, ymax, nny)
+X, Y = np.meshgrid(x,y)
+
+# Generate circular mask for interface
+interface_mask = (X - interface_center[0])**2 + (Y - interface_center[1])**2 <= interface_radius**2
 
 
-if __name__ == '__main__':
+def generate_random(i):
     # Parameters for data generation
     xmin, xmax, nnx = cfg['domain']['xmin'], cfg['domain']['xmax'], cfg['domain']['nnx']
     nny, ymin, ymax = cfg['domain']['nny'], cfg['domain']['ymin'], cfg['domain']['ymax']
-    interface_center = (cfg['domain']['interface_center']['x'], cfg['domain']['interface_center']['y'])
-    interface_radius = cfg['domain']['interface_radius']
-    n_res_factor = 16
+    n_res_factor = 20
 
     # Create a grid
     x, y= np.linspace(xmin, xmax, nnx), np.linspace(ymin, ymax, nny)
-    X, Y = np.meshgrid(x,y)
-
-    # Generate circular mask for interface
-    interface_mask = (X - interface_center[0])**2 + (Y - interface_center[1])**2 <= interface_radius**2
 
     # Factor to divide the grid by to generate the random grid
     nnx_lower = int(nnx / n_res_factor)
@@ -38,39 +45,42 @@ if __name__ == '__main__':
     x_lower, y_lower = np.linspace(xmin, xmax, nnx_lower), np.linspace(ymin, ymax, nny_lower)
     points = np.array(np.meshgrid(x, y, indexing='ij')).T.reshape(-1, 2)
 
-    def generate_random_data(nits):
-        for i in range(nits):
-            z_lower = 2 * np.random.random((nnx_lower, nny_lower)) - 1
-            f = rgi((x_lower, y_lower,), z_lower, method='cubic')
-            yield f(points).reshape((nnx, nny))
+    z_lower_inside = 2 * np.random.random((nnx_lower, nny_lower)) - 1
+    z_lower_outside = 2 * np.random.random((nnx_lower, nny_lower)) - 1
+    f_inside = rgi((x_lower, y_lower,), z_lower_inside, method='cubic')
+    f_outside = rgi((x_lower, y_lower,), z_lower_outside, method='cubic')
+    return f_inside(points).reshape((nnx, nny)), f_outside(points).reshape((nnx, nny))
+
+
+if __name__ == '__main__':
+    pool = Pool(processes=cpu_count())
 
     plots_dir = os.path.join('generated', 'plots')
     if not os.path.exists(plots_dir):
         os.makedirs(plots_dir)
 
+    print('nnx: ', cfg['domain']['nnx'], 'nny: ', cfg['domain']['nny'], "nnz: ", cfg['domain']['nnz'])
+
     # Generate random data samples
-    random_data_array = np.empty((nits, nnx, nny))
-    inside_domain_array = np.empty((nits, nnx, nny))
-    outside_domain_array = np.empty((nits, nnx, nny))
-    for idx, random_data in log_progress(enumerate(generate_random_data(nits)), total=nits, desc="Processing"):
-        if idx == 0:
-            inside_domain = random_data.copy()
-        else:
-            inside_domain = random_data_old.copy() 
-        outside_domain = random_data.copy() 
+    data_array = np.empty((nits, cfg['domain']['nnx'], cfg['domain']['nny']))
+    inside_domain_array = np.empty((nits, cfg['domain']['nnx'], cfg['domain']['nny']))
+    outside_domain_array = np.empty((nits, cfg['domain']['nnx'], cfg['domain']['nny']))
+    for idx, (inside_domain, outside_domain) in log_progress(enumerate(pool.imap(generate_random, range(nits))), total=nits, desc="Processing"):
 
         inside_domain[~interface_mask] = 0
         outside_domain[interface_mask] = 0
 
-        random_data_array[idx, interface_mask] = inside_domain[interface_mask] 
-        random_data_array[idx, ~interface_mask] = outside_domain[~interface_mask] 
+        data_array[idx, interface_mask] = inside_domain[interface_mask] 
+        data_array[idx, ~interface_mask] = outside_domain[~interface_mask] 
         inside_domain_array[idx] = inside_domain
         outside_domain_array[idx] = outside_domain
-        random_data_old = random_data
+        plt.figure(figsize=(8, 6))
+        plt.imshow(data_array[idx])
+        plt.show()
         
         if ploting and idx%100==0:
             plt.figure(figsize=(8, 6))
-            plt.imshow(random_data, extent=(xmin, xmax, ymin, ymax), origin='lower', cmap='viridis')
+            plt.imshow(data_array[idx], extent=(xmin, xmax, ymin, ymax), origin='lower', cmap='viridis')
             plt.colorbar(label='Random Data')
             plt.title(f'Random Data Sample {idx}')
             plt.xlabel('X')
@@ -82,6 +92,6 @@ if __name__ == '__main__':
     file_path_inside = os.path.join('generated', 'inside.npy')
     file_path_outside = os.path.join('generated', 'outside.npy')
     os.makedirs('generated', exist_ok=True)
-    np.save(file_path_domain, random_data_array)
+    np.save(file_path_domain, data_array)
     np.save(file_path_inside, inside_domain_array)
     np.save(file_path_outside, outside_domain_array)
