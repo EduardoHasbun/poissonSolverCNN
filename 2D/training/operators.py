e540f283602cd3137617d5c8a4b867672d137a8a
Eduardo Hasbun <hasbun.eduardo1999@gmail.com>
2024-05-29 23:15:27 -0400

    test_6

--- 2D/training/operators.py
+++ 2D/training/operators.py
@@ -61,7 +61,7 @@ class DirichletBoundaryLossFunction(nn.Module):
         def function2solve(x, y):
             return torch.pow(x,3) + torch.pow(y,3)
         
-        domain = function2solve(X, Y) * ratio_max
+        domain = function2solve(X, Y)
         self.domain = domain.unsqueeze(0)
 
     def forward(self, output, data_norm = 1.):
