cf74be9d4b8817622147354a36be15adb64f8427
Eduardo Hasbun <hasbun.eduardo1999@gmail.com>
2024-06-26 11:13:54 -0400

    3d

--- 3D/training/train3d.py
+++ 3D/training/train3d.py
@@ -94,7 +94,7 @@ for epoch in range (num_epochs):
         optimizer.zero_grad()
         data_norm = torch.ones((data.size(0), data.size(1), 1, 1)) / ratio_max
         output = model(data)
-        print(np.shape(output))
+        print(np.shape(data_norm))
         if loss_type =='laplacian':
             loss = laplacian_loss(output, data = data, data_norm = data_norm)
         elif loss_type == 'inside':
