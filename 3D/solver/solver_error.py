4c0c7abe22102c22f628d7aa54238a4aa1aca321
Eduardo Hasbun <hasbun.eduardo1999@gmail.com>
2024-07-30 18:14:04 -0400

    test3d 2

--- 3D/solver/solver_error.py
+++ 3D/solver/solver_error.py
@@ -56,7 +56,7 @@ if network_type == 'UNet':
     model = UNet(scales=scales, kernel_sizes=kernel_size, input_res=nnx)
 elif network_type == 'MSNet':
     model = MSnet(scales=scales, kernel_sizes=kernel_size, input_res=nnx)
-model.load_state_dict(torch.load('C:/Codigos/poissonSolverCNN/3D/training/models/model_11.pth'))
+model.load_state_dict(torch.load('C:/Codigos/poissonSolverCNN/3D/training/models/test3d_1.pth'))
 model = model.float()
 for param in model.parameters():
     param.data = param.data.float()
@@ -96,4 +96,4 @@ axs[2].set_ylabel('Y')
 cbar_output = plt.colorbar(img_error, ax=axs[2], label='Magnitude')
 plt.tight_layout()
 os.makedirs('results', exist_ok=True)
-plt.savefig(os.path.join(plots_dir, f'model3d_11.png'))
+plt.savefig(os.path.join(plots_dir, f'Test3d 1.png'))
