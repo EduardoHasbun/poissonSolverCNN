17ff963586c6e0fa597c1b4c1191cecd87f67cad
Eduardo Hasbun <hasbun.eduardo1999@gmail.com>
2024-08-08 00:14:29 -0400

    gpu

--- 2D/training/train_gpu.py
+++ 2D/training/train_gpu.py
@@ -1,5 +1,5 @@
 import torch
-if torch.cuda.is_available():
-    print(f"GPU: {torch.cuda.get_device_name(0)} is available.")
-else:
-    print("No GPU available. Training will run on CPU.")
+print(torch.cuda.is_available())  # Should return True
+print(torch.cuda.current_device())  # Should return 0 or other device number
+print(torch.cuda.device_count())  # Should return number of GPUs
+print(torch.cuda.get_device_name(0))  # Should return the name of your GPU
