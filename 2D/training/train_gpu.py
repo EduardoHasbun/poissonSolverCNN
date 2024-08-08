361b996c3f4615fcd50ea2a3f5216078f671ee71
Eduardo Hasbun <hasbun.eduardo1999@gmail.com>
2024-08-08 00:12:27 -0400

    gpu

--- 2D/training/train_gpu.py
+++ 2D/training/train_gpu.py
@@ -1,2 +1,5 @@
-import tensorflow as tf
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+import torch
+if torch.cuda.is_available():
+    print(f"GPU: {torch.cuda.get_device_name(0)} is available.")
+else:
+    print("No GPU available. Training will run on CPU.")
