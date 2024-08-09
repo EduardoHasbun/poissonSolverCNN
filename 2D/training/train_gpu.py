ca18d102df0c6ce9c325fba79fa6068186ab3255
Eduardo Hasbun <hasbun.eduardo1999@gmail.com>
2024-08-09 18:36:25 -0400

    gpu

--- 2D/training/train_gpu.py
+++ 2D/training/train_gpu.py
@@ -81,3 +81,4 @@ for epoch in range(num_epochs):
     for batch_idx, batch in enumerate(dataloader):
         data = batch[0].unsqueeze(1).to(device)
         data_norm = torch.ones((data.size(0), data.size(1), 1, 1))
+        print(batch_idx)
