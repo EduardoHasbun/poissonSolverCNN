9cdfe360fa28ff0af5089a35636b5f0c490c76bd
Eduardo Hasbun <hasbun.eduardo1999@gmail.com>
2024-09-05 09:57:21 -0400

    interface 12

--- 2D/solver/solver_error.py
+++ 2D/solver/solver_error.py
@@ -49,14 +49,14 @@ alpha = 0.1
 ratio_max = ratio_potrhs(alpha, Lx, Ly)
 
 # Create input data and resolution data for the error
-input_data = function2solve(X, Y)
+input_data = function2solve(X, Y) * ratio_max
 input_data = input_data[np.newaxis, np.newaxis, :, :] 
 input_data = torch.from_numpy(input_data).float()
 resolution_data = resolution(X, Y)
 
 # Create Model
 model = UNet(scales, kernel_sizes=kernel_size, input_res=nnx)
-model.load_state_dict(torch.load('C:/Codigos/poissonSolverCNN/2D/training/models/test2d_4.pth'))
+model.load_state_dict(torch.load('C:/Codigos/poissonSolverCNN/2D/training/models/test2d_1.pth'))
 model = model.float()
 model.eval() 
 
@@ -91,4 +91,4 @@ axs[2].set_ylabel('Y')
 cbar_output = plt.colorbar(img_error, ax=axs[2], label='Magnitude')
 plt.tight_layout()
 os.makedirs('results', exist_ok=True)
-plt.savefig(os.path.join(plots_dir, f'Test_2D 4.png'))
+plt.savefig(os.path.join(plots_dir, f'Test_2D 1_1.png'))
