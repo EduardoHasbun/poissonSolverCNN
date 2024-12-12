16e76a21da329065fb0d90ccfc50f0369571f5dd
Eduardo Hasbun <hasbun.eduardo1999@gmail.com>
2024-12-12 11:33:54 -0300

    test

--- 3D/dataset/random_charge_interface.py
+++ 3D/dataset/random_charge_interface.py
@@ -1,98 +1,106 @@
 import numpy as np
-from numpy import pi
-from scipy import special
-import pyvista as pv
+from scipy import special as sp
 
-def an_spherical_field(q, xq, E_1, E_2, E_0, R, N, points):
-    xq = np.array(xq)
-    q = np.array(q)
-    points = np.array(points)
-    Nq = len(q)
-    Np = len(points)
-    
-    # Precompute spherical coords for charges
-    rho_q = np.linalg.norm(xq, axis=1)
-    with np.errstate(divide='ignore', invalid='ignore'):
-        theta_q = np.arccos(np.where(rho_q > 1e-15, xq[:, 2] / rho_q, 1.0))
-        phi_q = np.arctan2(xq[:, 1], xq[:, 0])
-    phi_q[rho_q < 1e-15] = 0.0
-    theta_q[rho_q < 1e-15] = 0.0
-    
-    # Precompute spherical harmonics for charges
-    Ynm_charges = {}
-    for n in range(N):
-        for m in range(-n, n + 1):
-            Ynm_charges[(n, m)] = special.sph_harm(m, n, phi_q, theta_q)
-    
-    PHI = np.zeros(Np, dtype=float)
-    
-    for iP in range(Np):
-        x, y, z = points[iP]
-        rr = np.sqrt(x * x + y * y + z * z)
-        if rr < 1e-15:
-            theta = 0.0
-            phi = 0.0
-        else:
-            theta = np.arccos(z / rr)
-            phi = np.arctan2(y, x)
+
+class Solution_utils():
+    def __init__(self, qs, x_qs, epsilon_1, epsilon_2, kappa):
+        self.qs = qs
+        self.x_qs = x_qs
+        self.epsilon_1 = epsilon_1
+        self.epsilon_2 = epsilon_2
+        self.kappa = kappa
+        self.mesh = type('Mesh', (), {'R_max_dist': 10.0})  
+
+
+    def Spherical_Harmonics(self, x, flag, R=None, N=25):
+
+        q = self.qs
+        xq = self.x_qs
+        E_1 = self.epsilon_1
+        E_2 = self.epsilon_2
+        kappa = self.kappa
+        if R is None:
+            R = self.mesh.R_max_dist
+
+        PHI = np.zeros(len(x))
+
+        for K in range(len(x)):
+            rho = np.sqrt(np.sum(x[K,:] ** 2))
+            zenit = np.arccos(x[K, 2] / rho)
+            azim = np.arctan2(x[K, 1], x[K, 0])
+
+            phi = 0.0 + 0.0 * 1j
+
+            for n in range(N):
+                for m in range(-n, n + 1):
+
+                    Enm = 0.0
+                    for k in range(len(q)):
+                        rho_k = np.sqrt(np.sum(xq[k,:] ** 2))
+                        zenit_k = np.arccos(xq[k, 2] / rho_k)
+                        azim_k = np.arctan2(xq[k, 1], xq[k, 0])
+
+                        Enm += (
+                            q[k]
+                            * rho_k**n
+                            *4*np.pi/(2*n+1)
+                            * sp.sph_harm(m, n, -azim_k, zenit_k)
+                        )
+
+                    Anm = Enm * (1/(4*np.pi)) * ((2*n+1)) / (np.exp(-kappa*R)* ((E_1-E_2)*n*self.get_K(kappa*R,n)+E_2*(2*n+1)*self.get_K(kappa*R,n+1)))
+                    Bnm = 1/(R**(2*n+1))*(np.exp(-kappa*R)*self.get_K(kappa*R,n)*Anm - 1/(4*np.pi*E_1)*Enm)
+                    
+                    if flag=='molecule':
+                        phi += Bnm * rho**n * sp.sph_harm(m, n, azim, zenit)
+                    if flag=='solvent':
+                        phi += Anm * rho**(-n-1)* np.exp(-kappa*rho) * self.get_K(kappa*rho,n) * sp.sph_harm(m, n, azim, zenit)
+
+            PHI[K] = np.real(phi)
         
-        phi_field = 0.0 + 0.0j
-        for nn in range(N):
-            for mm in range(-nn, nn + 1):
-                Ynm_field = special.sph_harm(mm, nn, phi, theta)
-                cons1 = ((rr**nn) * (E_1 - E_2) * (nn + 1)) / (
-                    E_1 * E_0 * (R**(2 * nn + 1)) * (E_1 * nn + E_2 * (nn + 1))
-                )
-                cons2 = 4 * pi / (2 * nn + 1)
-                
-                sum_over_charges = np.sum(q * (rho_q**nn) * np.conjugate(Ynm_charges[(nn, mm)]))
-                phi_field += cons1 * cons2 * Ynm_field * sum_over_charges
-        
-        PHI[iP] = np.real(phi_field) / (4 * pi)
-    
-    return PHI
+        return PHI
+    @staticmethod
+    def get_K(x, n):
+        K = 0.0
+        n_fact = sp.factorial(n)
+        n_fact2 = sp.factorial(2 * n)
+        for s in range(n + 1):
+            K += (
+                2 ** s
+                * n_fact
+                * sp.factorial(2 * n - s)
+                / (sp.factorial(s) * n_fact2 * sp.factorial(n - s))
+                * x ** s
+            )
+        return K
 
-if __name__ == "__main__":
-    # Sphere parameters
-    R = 1.0             # radius
-    E_0 = 8.854e-12      # vacuum permittivity
-    E_1 = 80.0           # dielectric inside
-    E_2 = 1.0            # dielectric outside
-    N_expansion = 10     # expansion order
-    
-    # One charge inside the sphere
-    q = [1.0e-19]
-    xq = [[0.3 * R, 0.0, 0.0]]
-    
-    # Create a 3D grid of points
-    ngrid = 50
-    lin = np.linspace(-R, R, ngrid)
-    X, Y, Z = np.meshgrid(lin, lin, lin)
-    
-    # Mask points inside the sphere
-    inside_mask = (X**2 + Y**2 + Z**2) <= R**2
-    # Extract only inside points
-    X_in = X[inside_mask]
-    Y_in = Y[inside_mask]
-    Z_in = Z[inside_mask]
-    points_in = np.vstack((X_in, Y_in, Z_in)).T
-    
-    # Compute potential inside the sphere
-    PHI_in = an_spherical_field(q, xq, E_1, E_2, E_0, R, N_expansion, points_in)
-    
-    # Store results in a 3D array
-    PHI_3D = np.full((ngrid, ngrid, ngrid), np.nan)
-    PHI_3D[inside_mask] = PHI_in
-    
-    # Create a PyVista grid for visualization
-    grid = pv.UniformGrid()
-    grid.dimensions = PHI_3D.shape
-    grid.origin = (-R, -R, -R)
-    grid.spacing = (2 * R / (ngrid - 1), 2 * R / (ngrid - 1), 2 * R / (ngrid - 1))
-    grid.point_data["Potential"] = PHI_3D.ravel(order="F")  # PyVista expects Fortran ordering
 
-    # Visualize with PyVista
-    pl = pv.Plotter()
-    pl.add_volume(grid, scalars="Potential", cmap="viridis", opacity="sigmoid")
-    pl.add_axes()
-    pl.show()
+qs = [1.0, -1.0]
+x_qs = np.array([[0.0, 0.0, 1.0], [0.0, 0.0, -1.0]])
+epsilon_1 = 80.0
+epsilon_2 = 1.0
+kappa = 0.1
+
+solution = Solution_utils(qs, x_qs, epsilon_1, epsilon_2, kappa)
+
+
+grid_size = 60
+x = np.linspace(-5, 5, grid_size)
+y = np.linspace(-5, 5, grid_size)
+z = np.linspace(-5, 5, grid_size)
+X, Y, Z = np.meshgrid(x, y, z)
+points = np.column_stack((X.ravel(), Y.ravel(), Z.ravel()))
+
+
+field = solution.Spherical_Harmonics(points, flag='molecule', N = 20)
+
+
+field_reshaped = field.reshape(grid_size, grid_size, grid_size)
+
+import matplotlib.pyplot as plt
+
+plt.imshow(field_reshaped[:, :, grid_size//2])
+plt.title("3D Spherical Harmonics Field")
+plt.savefig('pll.png')
+
+
+
