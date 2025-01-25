a27498d718b40a682777b234cc0964dd164304e6
Eduardo Hasbun <hasbun.eduardo1999@gmail.com>
2025-01-25 00:10:57 -0300

    Train interface 3d 3

--- 3D/training/train_interface.py
+++ 3D/training/train_interface.py
@@ -126,5 +126,3 @@ for epoch in range (num_epochs):
             print(f"Epoch {epoch}, Batch {batch_idx}, Loss: {loss.item()}")
     print(f"Epoch [{epoch + 1}/{num_epochs}] - Loss: {total_loss / len(dataloader)}")
     torch.save(model.state_dict(), os.path.join(save_dir, case_name))
-
-
