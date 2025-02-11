b9561d69fd1a513ed462c40ee66eee9bd8b606fc
Eduardo Hasbun <hasbun.eduardo1999@gmail.com>
2025-02-10 23:02:11 -0300

    interface 3d 5

--- 3D/training/train_interface.py
+++ 3D/training/train_interface.py
@@ -96,12 +96,23 @@ interface_loss = InterfaceBoundaryLoss(interface_weight, interface_boundary, int
 inside_loss = InsideLossInterface(cfg, inside_weight)
 optimizer = optim.Adam(model.parameters(), lr=lr)
 
+# Initialize lists to store losses
+epoch_losses = []  # To store total losses per epoch
+batch_losses = []  # To store batch losses
+laplacian_losses_in = []  # To store Laplacian losses inside
+laplacian_losses_out = []  # To store Laplacian losses outside
+dirichlet_losses = []  # To store Dirichlet losses
+interface_losses = []  # To store Interface losses
+
 
 print(f"Model used: {cfg['arch']['arch_dir']}, {arch_type}")
-
 # Train loop
 for epoch in range (num_epochs):
     total_loss = 0
+    epoch_laplacian_loss_in = 0
+    epoch_laplacian_loss_out = 0
+    epoch_dirichlet_loss = 0
+    epoch_interface_loss = 0
     for batch_idx, batch in enumerate(dataloader):
         data, target = batch
         data = data.unsqueeze(1)
@@ -113,19 +124,60 @@ for epoch in range (num_epochs):
         subdomain_in, subdomain_out = model(data)
 
         # Loss
-        loss = laplacian_loss(subdomain_in, data, data_norm, inner_mask)
-        loss += laplacian_loss(subdomain_out, data, data_norm, outer_mask)
-        loss += interface_loss(subdomain_in, subdomain_out)
-        loss += dirichlet_loss(subdomain_out)
+        laplacian_loss_in_value = laplacian_loss(subdomain_in, data, data_norm, inner_mask)
+        laplacian_loss_out_value = laplacian_loss(subdomain_out, data, data_norm, outer_mask)
+        interface_loss_value = interface_loss(subdomain_in, subdomain_out)
+        dirichlet_loss_value = dirichlet_loss(subdomain_out)
+        loss = laplacian_loss_in_value + laplacian_loss_out_value + dirichlet_loss_value + interface_loss_value
 
         # Backpropagation
         loss.backward()
         optimizer.step()
+
+        # Update total loss
         total_loss += loss.item()
+        epoch_laplacian_loss_in += laplacian_loss_in_value.item()
+        epoch_laplacian_loss_out += laplacian_loss_out_value.item()
+        epoch_dirichlet_loss += dirichlet_loss_value.item()
+        epoch_interface_loss += interface_loss_value.item()
+
+        # Save batch losses
+        batch_losses.append(loss.item())
+        laplacian_losses_in.append(laplacian_loss_in_value.item())
+        laplacian_losses_out.append(laplacian_loss_out_value.item())
+        interface_losses.append(interface_loss_value.item())
+        dirichlet_losses.append(dirichlet_loss_value.item())
+
         if batch_idx % 20 ==0:
             print(f"Epoch {epoch}, Batch {batch_idx}, Loss: {loss.item()}")
             
-    print(f"Epoch [{epoch + 1}/{num_epochs}] - Loss: {total_loss / len(dataloader)}")
+    # Save epoch losses
+    epoch_losses.append(total_loss / len(dataloader))
+    print(f"Epoch [{epoch + 1}/{num_epochs}] - Total Loss: {total_loss / len(dataloader)}")
     torch.save(model.state_dict(), os.path.join(save_dir, case_name))
     if epoch % 10 == 0:
         torch.save(model.state_dict(), os.path.join(save_dir, case_name + f'_epoch_{epoch}'))
+
+
+# Save losses to a .txt file
+loss_file_path = os.path.join(save_dir, f"{case_name}_losses.txt")
+with open(loss_file_path, "w") as f:
+    f.write("Epoch Losses:\n")
+    f.write(", ".join(map(str, epoch_losses)) + "\n\n")
+    
+    f.write("Batch Losses:\n")
+    f.write(", ".join(map(str, batch_losses)) + "\n\n")
+    
+    f.write("Laplacian Losses_inside:\n")
+    f.write(", ".join(map(str, laplacian_losses_in)) + "\n\n")
+
+    f.write("Laplacian Losses_outside:\n")
+    f.write(", ".join(map(str, laplacian_losses_out)) + "\n\n")
+
+    f.write("Interface Losses:\n")
+    f.write(", ".join(map(str, interface_losses)) + "\n\n")
+    
+    f.write("Dirichlet Losses:\n")
+    f.write(", ".join(map(str, dirichlet_losses)) + "\n")
+
+print(f"Losses saved to {loss_file_path}")
