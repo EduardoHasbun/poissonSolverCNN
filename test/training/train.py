58614a50f4e9feb16fdd23d47d4426679764eab0
EduardoHasbun <hasbun.eduardo1999@gmail.com>
2025-04-07 16:45:04 -0400

    interface combined model

--- test/training/train.py
+++ test/training/train.py
@@ -172,7 +172,7 @@ dataloader = DataLoader(dataset,
 model = UNet3DCombined(
     scales=scales,
     kernel_sizes=kernel_size,
-    input_res=(nnx, nny, nnz)
+    input_res=[nnx, nny, nnz]
 ).double().to(device)
 
 laplacian_loss = LaplacianLossInterface(cfg, lapl_weight, inner_mask, outer_mask, points)
