d17e83f61ff218fbf139ffd1bd85ebda7366655f
EduardoHasbun <hasbun.eduardo1999@gmail.com>
2025-04-07 21:16:21 -0400

    combined interface model

--- test/training/operators.py
+++ test/training/operators.py
@@ -247,8 +247,12 @@ class InterfaceBoundaryLoss(nn.Module):
         gc_normal_deriv = (gc_grad_boundary[:, 0] * self.normal_x
                         + gc_grad_boundary[:, 1] * self.normal_y
                         + gc_grad_boundary[:, 2] * self.normal_z)
-        loss += F.mse_loss(self.e_in * (normal_derivate_inner[:, self.boundary]+ gc_normal_deriv.unsqueeze(0)),
-                         self.e_out * normal_derivate_outer[:, self.boundary])
+        lhs = self.e_in * (
+            normal_derivate_inner[:, self.boundary] 
+            + gc_normal_deriv.unsqueeze(0)          
+        )
+        rhs = self.e_out * normal_derivate_outer[:, self.boundary]
+        loss += F.mse_loss(lhs, rhs)
 
         return loss * self.weight
 
