b27bd53d734b6e4e6bf4f9b9455aec8774d8e2f1
EduardoHasbun <hasbun.eduardo1999@gmail.com>
2025-06-02 01:04:57 -0400

    3d dirichlet unet 200

--- 3D/solver/solver.py
+++ 3D/solver/solver.py
@@ -80,7 +80,7 @@ resolution_data = poisson_punctual_solution(X, Y, Z, punctual_charges)
 def ratio_potrhs(alpha, Lx, Ly, Lz):
     return alpha / (np.pi**2 / 8)**2 / (1 / Lx**2 + 1 / Ly**2 + 1 / Lz**2)
 
-alpha = 0.5
+alpha = 0.12
 ratio_max = ratio_potrhs(alpha, Lx, Ly, Lz)
 
 # Generar entrada
